3e31bcee244d5a78bcde5dae4ebf690b42256393
Chandler Cox <mr.chandlercox@gmail.com>
2024-09-06 13:13:01 -0500

    Fix compile error with Scripting::Update. Changed to Engine::Update.

--- Source/ArizonaFramework/Debug/DebugSystem.cpp
+++ Source/ArizonaFramework/Debug/DebugSystem.cpp
@@ -31,13 +31,13 @@ DebugSystem::DebugSystem(const SpawnParams& params)
 
 void DebugSystem::Initialize()
 {
-    Scripting::Update.Bind<DebugSystem, &DebugSystem::OnUpdate>(this);
+    Engine::Update.Bind<DebugSystem, &DebugSystem::OnUpdate>(this);
     _menuActive = false;
 }
 
 void DebugSystem::Deinitialize()
 {
-    Scripting::Update.Unbind<DebugSystem, &DebugSystem::OnUpdate>(this);
+    Engine::Update.Unbind<DebugSystem, &DebugSystem::OnUpdate>(this);
     _windows.ClearDelete();
 }
 
